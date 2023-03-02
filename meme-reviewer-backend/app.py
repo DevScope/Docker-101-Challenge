f1df0567fdbcb55d79549c6882a93e27ecdfc09d
Vitor Correia <vitorjcorreia@outlook.com>
2023-03-02 16:41:35 +0000

    Multiple improvements backend and frontend

--- meme-reviewer-backend/app.py
+++ meme-reviewer-backend/app.py
@@ -1,18 +1,22 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
+import uuid
+import base64
 
-meme_info = [
-    { "name": "Change my mind", "imagePath": "https://img-9gag-fun.9cache.com/photo/aeMe6WW_700bwp.webp", "year": "2018" },
-    { "name": "Success Kid", "imagePath": "https://i.pinimg.com/736x/91/92/07/91920700c1d95a3348382cf60b27673a.jpg", "year": "2016" },
-    { "name": "Shut up and take my money", "imagePath": "https://i.kym-cdn.com/entries/icons/original/000/005/574/takemymoney.jpg", "year": "2016" },
-    { "name": "That Escalated Quickly", "imagePath": "https://cdn.vox-cdn.com/thumbor/CKkF4aPYdSRsv9EiaQFs_PnlArs=/0x0:500x577/1200x0/filters:focal(0x0:500x577):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/6367395/f17.jpg", "year": "2004" },
-    { "name": "Disaster girl", "imagePath": "https://www.thesun.co.uk/wp-content/uploads/2016/07/nintchdbpict000252984341.jpg", "year": "2004" },
-    { "name": "Exit 12", "imagePath": "https://i.kym-cdn.com/photos/images/newsfeed/001/329/047/9a2.png", "year": "2004" },
-    { "name": "Distracted boyfriend", "imagePath": "https://i.kym-cdn.com/photos/images/newsfeed/001/287/547/e06.jpg", "year": "2015" },
-    { "name": "Doge", "imagePath": "https://thejesuitpost.org/wp-content/uploads/2014/05/Doge-natius.jpg", "year": "2010" },
-    { "name": "First World Problems", "imagePath": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmQSsVaN4blxQNCuT-ZBCYRSnvYRfkprQj3g&usqp=CAU", "year": "1995" }
-]
+# meme_info = [
+#     { "name": "Change my mind", "imagePath": "https://img-9gag-fun.9cache.com/photo/aeMe6WW_700bwp.webp", "year": "2018" },
+#     { "name": "Success Kid", "imagePath": "https://i.pinimg.com/736x/91/92/07/91920700c1d95a3348382cf60b27673a.jpg", "year": "2016" },
+#     { "name": "Shut up and take my money", "imagePath": "https://i.kym-cdn.com/entries/icons/original/000/005/574/takemymoney.jpg", "year": "2016" },
+#     { "name": "That Escalated Quickly", "imagePath": "https://cdn.vox-cdn.com/thumbor/CKkF4aPYdSRsv9EiaQFs_PnlArs=/0x0:500x577/1200x0/filters:focal(0x0:500x577):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/6367395/f17.jpg", "year": "2004" },
+#     { "name": "Disaster girl", "imagePath": "https://www.thesun.co.uk/wp-content/uploads/2016/07/nintchdbpict000252984341.jpg", "year": "2004" },
+#     { "name": "Exit 12", "imagePath": "https://i.kym-cdn.com/photos/images/newsfeed/001/329/047/9a2.png", "year": "2004" },
+#     { "name": "Distracted boyfriend", "imagePath": "https://i.kym-cdn.com/photos/images/newsfeed/001/287/547/e06.jpg", "year": "2015" },
+#     { "name": "Doge", "imagePath": "https://thejesuitpost.org/wp-content/uploads/2014/05/Doge-natius.jpg", "year": "2010" },
+#     { "name": "First World Problems", "imagePath": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmQSsVaN4blxQNCuT-ZBCYRSnvYRfkprQj3g&usqp=CAU", "year": "1995" }
+# ]
+
+memes = []
 
 app = Flask(__name__)
 environment = os.getenv('APP_ENVIRONMENT')
@@ -24,5 +28,23 @@ def health():
     return 'Meme reviewer is running!'
 
 @app.route('/api/meme')
-def get_meme_info():
-    return jsonify(meme_info)
+def get_meme():
+    return jsonify(memes)
+
+@app.route('/api/meme', methods=['POST'])
+def post_meme():
+    file = request.files['image']
+    ext = file.filename.split(".")[-1]
+    # filename = f'./images/{uuid.uuid4()}.{file.filename.split(".")[-1]}'
+    # file.save(filename)
+
+    rv = base64.b64encode(file.read())
+    rv = rv.decode('ascii')
+        
+    memes.append({ 
+        'name': request.form['name'],
+        'data': f'data:image/{ext};base64,{rv}',
+        'year': request.form['year']
+    })
+    
+    return Response("", status=201)
--- meme-reviewer-frontend/src/components/MemeList.js
+++ meme-reviewer-frontend/src/components/MemeList.js
@@ -2,40 +2,80 @@ import React, { useEffect, useState } from "react";
 import { Card, Row, Col, Container } from "react-bootstrap";
 import axios from 'axios';
 
-const host = process.env.REACT_APP_BACKEND_HOST
-    ? process.env.REACT_APP_BACKEND_HOST : 'localhost';
-
-const port = process.env.REACT_APP_BACKEND_PORT
-    ? process.env.REACT_APP_BACKEND_PORT : '5000';
+const backendUrl = process.env.REACT_APP_BACKEND_URL
+    ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:5000';
 
 export const MemeList = () => {
+    const [name, setName] = useState('');
+    const [year, setYear] = useState('');
     const [memes, setMemes] = useState([]);
+    const [image, setImage] = useState(null);
 
     useEffect(() => {
         const asyncCallback = async () => {
-            let response = await axios.get(`http://${host}:${port}/api/meme`);
-            console.log(response.data)
+            let response = await axios.get(`${backendUrl}/api/meme`);
             return response.data
         }
 
         let data = asyncCallback();
         data.then(info => setMemes(info))
-    }, []);
+    }, [name, year, image]);
+
+    function onNameChange(event) {
+        setName(event.target.value)
+    }
+
+    function onYearChange(event) {
+        setYear(event.target.value)
+    }
+
+    function onImageChange(event) {
+        setImage(event.target.files[0])
+    }
+
+    function onImageUpload(e) {
+        e.preventDefault()
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('year', year);
+        formData.append(
+            "image",
+            image
+        );
+
+        setName('');
+        setYear('');
+        setImage(null);
+        axios.post(`http://${host}:${port}/api/meme`, formData);
+    }
 
     return (
         <div>
             <nav id="navbar" class="navbar navbar-light bg-dark">
                 <a title="Click here!" id="logo" class="navbar-brand" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-                    <img src="trollface.png" width="100" height="75"  alt="" />
+                    <img src="trollface.png" width="100" height="75" alt="" />
                 </a>
                 <h1 class="navbar-brand text-light">Meme Reviewer</h1>
             </nav>
+            <div style={{ 'display': 'block', 'paddingTop': '0.5cm', 'paddingLeft': '0.5cm', 'width': '30%' }}>
+                {/* <label class="form-label" for="customFile">Upload your meme!</label> */}
+                <label class="form-label" for="customName">Name</label>
+                <input type="text" class="form-control" id="name" name="name" onChange={onNameChange} />
+                <label class="form-label" for="customYear">Year</label>
+                <input type="text" class="form-control" id="year" name="year" onChange={onYearChange} />
+                <div class="input-group mb-3" style={{ 'paddingTop': '0.5cm'}}>
+                    <input type="file" class="form-control" id="customFile" name="image" onChange={onImageChange} />
+                    <div class="input-group-append" style={{ 'paddingLeft': '0.2cm' }}>
+                        <button class="btn btn-primary" type="submit" onClick={onImageUpload}>Submit</button>
+                    </div>
+                </div>
+            </div>
             <Container>
-                <Row style={{padding: '5%'}}>
+                <Row style={{ padding: '5%' }}>
                     {memes.map((meme, k) => (
                         <Col key={k} xs={12} md={5} lg={3}>
                             <Card className="text-white bg-dark mb-3">
-                                <Card.Img src={meme.imagePath} />
+                                <Card.Img src={meme.data} />
 
                                 <Card.Body>
                                     <Card.Title>{meme.name}</Card.Title>
